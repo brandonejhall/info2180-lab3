c2cff876a01c3d7dfa4bb37ae8707528d2f4a0d2
brandonejhall <brandonejhall7@outlook.com>
2021-10-09 01:38:49 -0500

    Excersise-2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,40 @@
 "use strict";
 
 window.onload = function(){
-    var square =  document.getElementById("board").querySelectorAll("div");
+    var box =  document.getElementById("board").querySelectorAll("div");
+    var board = document.getElementById("board")
+    console.log(box);
+    
+    let counter = 0;
 
-    square.forEach(function(item)
+    board.onclick = function(){
+        counter++;
+        console.log(counter);
+    }
+    
+    box.forEach(function(item)
     {
         item.setAttribute("class","square");
+        item.innerHTML="";
+        let game_progress = []
+        
+        
+        item.addEventListener('click',function(){
+          
+        
+          if (counter % 2 == 0 && this.innerHTML == ""){
+              item.innerHTML = "X";
+              item.setAttribute("class", "square X")
+              
+          }
+          else if(counter % 2 != 0 && this.innerHTML == ""){
+              item.innerHTML = "O";
+              item.setAttribute("class", "square O")
+              
+          }
+        });
     });
-
-   
+    
+    
+    
 };
